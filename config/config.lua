95ed740663eab8d0f5acbc897a250c333aab16b6
Yoshi <113165101+y0sHiiiiiiii@users.noreply.github.com>
2022-09-20 20:48:46 +0200

    Create config.lua

---
+++ config/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+Config.Triggerlist = {
+  {
+    event = 'esx_ambulancejob:revive',
+    whook = 'WEBHOOK HIER REIN',                      -- Your Webhook
+  },
+  {
+    event = 'esx_taxijob:getMoney',
+    whook = 'WEBHOOK HIER REIN',                      -- Your Webhook
+  },
+}
